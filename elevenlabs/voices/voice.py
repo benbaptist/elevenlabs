10467c8fc20ffe031a53a29722589606361f578b
Ben Baptist <sawham6@gmail.com>
2023-03-13 15:35:28 -0400

    added some comments for help()

--- elevenlabs/voices/voice.py
+++ elevenlabs/voices/voice.py
@@ -11,28 +11,35 @@ class Voice:
 
     @property
     def id(self):
+        """ Returns the UUID of this Voice """
         return self._obj["voice_id"]
 
     @property
     def name(self):
+        """ Returns the name of this Voice """
         return self._obj["name"]
 
     @property
     def category(self):
+        """ Returns the category of this Voice """
         return self._obj["category"]
 
     @property
     def preview_url(self):
+        """ Returns a URL pointing to a sample audio file using this voice """
         return self._obj["preview_url"]
 
     @property
     def preview(self):
+        """ Returns an AudioFile object containing a preview of this voice """
         request = requests.get(self.preview_url)
 
         return AudioFile(request)
 
     @property
     def settings(self):
+        """ Returns all possible settings that you can configure with
+        this voice """
         request = self._request(
             "GET",
             "voices/%s/settings" % self.id
@@ -41,6 +48,7 @@ class Voice:
         return request.json()
 
     def generate(self, text, voice_settings=None):
+        """ Generate a text-to-speech with the provided text and settings """
         if not voice_settings:
             voice_settings = self.settings
 
